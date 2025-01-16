26c33652d15d6a0589e3f3b0bab32cbf8f753a94
gvega001 <gvega001@gmail.com>
2025-01-15 19:51:04 -0800

    Header implemented

--- C-Spark/utils.h
+++ C-Spark/utils.h
@@ -1,1 +1,13 @@
-#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Safe malloc wrapper
+void* safe_malloc(size_t size);
+
+// Safe realloc wrapper
+void* safe_realloc(void* ptr, size_t size);
+
+#endif // UTILS_H
