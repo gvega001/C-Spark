716fb980e1f55be21f1cef8c3cd02ac7d427b600
gvega001 <gvega001@gmail.com>
2025-02-01 12:21:37 -0800

    error reporting

--- C-Spark/error_reporting.c
+++ C-Spark/error_reporting.c
@@ -1,4 +1,3 @@
-// error_reporting.c
 #include "error_reporting.h"
 #include "utils.h"
 #include <stdio.h>
@@ -8,35 +7,56 @@
 #define COLOR_YELLOW "\033[1;33m"
 #define COLOR_RESET "\033[0m"
 
-// Global error log and counter
+// Global error storage
 Error error_log[MAX_ERRORS];
 int error_count = 0;
 
-// Centralized error reporting function
+// Backward Compatible Error Reporting
+#ifdef LEGACY_MODE
+void report_error(int line, int column, const char* message) {
+    fprintf(stderr, COLOR_RED "[Error] Line %d, Column %d: %s\n" COLOR_RESET, line, column, message);
+    log_error(line, column, message);
+}
+#else
 void report_error(const char* stage, int line, int column, const char* message) {
     fprintf(stderr, COLOR_RED "[%s Error] Line %d, Column %d: %s\n" COLOR_RESET, stage, line, column, message);
     log_error(line, column, message);
 }
+#endif
 
-// Report a warning (color–coded)
+// Log warnings
 void report_warning(int line, int column, const char* message) {
     fprintf(stderr, COLOR_YELLOW "  Warning (Line %d, Column %d): %s\n" COLOR_RESET, line, column, message);
 }
 
-// Log errors for later review
+// Backward Compatible Error Logging
+#ifdef LEGACY_MODE
 void log_error(int line, int column, const char* message) {
     if (error_count < MAX_ERRORS) {
         error_log[error_count].line = line;
         error_log[error_count].column = column;
-        error_log[error_count].message = utils_safe_strdup(message);
+        error_log[error_count].message = strdup(message);
         error_count++;
     }
     else {
         fprintf(stderr, "Error log is full. Unable to track more errors.\n");
     }
 }
+#else
+void log_error(int line, int column, const char* message) {
+    if (error_count < MAX_ERRORS) {
+        error_log[error_count].line = line;
+        error_log[error_count].column = column;
+        error_log[error_count].message = utils_safe_strdup(message);  // Uses safe memory allocation
+        error_count++;
+    }
+    else {
+        fprintf(stderr, "Error log is full. Unable to track more errors.\n");
+    }
+}
+#endif
 
-// Print all logged errors
+// Print all errors
 void print_error_log() {
     for (int i = 0; i < error_count; i++) {
         fprintf(stderr, COLOR_RED "Error %d: Line %d, Column %d: %s\n" COLOR_RESET,
@@ -44,7 +64,7 @@ void print_error_log() {
     }
 }
 
-// Free memory allocated for error messages
+// Free allocated error memory
 void free_error_log() {
     for (int i = 0; i < error_count; i++) {
         free(error_log[i].message);
